54aeb17c896d979e8c14faa05ae8fa9588c616b1
AlmazCode <104347420+AlmazCode@users.noreply.github.com>
2024-04-02 20:34:58 +0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from interpretator import Interpretator
-
-code: str = ""
-
-with open("code.bl", "r", encoding = "utf-8") as file:
-    code = file.read()
-
-BiLang = Interpretator(code)
-BiLang.begin()
